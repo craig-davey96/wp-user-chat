fff3d51ba0c17aabad42bb67bf8cc5e3e010b989
Craig Davey <craig96@talktalk.net>
2021-09-30 21:42:51 +0100

    added the version

--- cd-wp-user-chat.php
+++ cd-wp-user-chat.php
@@ -5,7 +5,8 @@
      * Plugin Name: WP User Chat
      * Plugin URI: https://github.com/craig-davey96/wp-user-chat
      * Author: Craig Davey
-     * Author URI: https://github.com/craig-davey96git
+     * Author URI: https://github.com/craig-davey96
+     * Version: 1.0
      * Description: Chat UI for the users in the Wordpress installation. Users can chat between each other using this plugin.
      * Tags: Wordpress,Chat,Users
      *
