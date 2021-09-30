e76fa96882ca10d043b63a469c39de82fd0ef3d6
Craig Davey <craig96@talktalk.net>
2021-09-30 22:00:27 +0100

    adding the admin pages files

--- class.wp-user-chat.php
+++ class.wp-user-chat.php
@@ -8,10 +8,16 @@
 
         }
 
-        public static function includeFilesInIncludes(){
+        public static function includeFilesInIncludes(){ // Include all the files inside the includes folder
 
+            include WPUC_INC.'class.admin-pages.php';
 
+        }
 
+        public static function getView($view){ // Get a view from the views folder
+            ob_start();
+            include WPUC_VIEWS.$view;
+            return ob_get_clean();
         }
 
     }
